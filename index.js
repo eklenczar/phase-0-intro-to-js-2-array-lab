590830525fa1723ea995f508bb59b9863f603bce
Elizabeth Klenczar <eklenczar@gmail.com>
2022-01-23 16:30:34 -0500

    in progress

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(){
+    const appendCat = [...cats, "Broom"];
+    return appendCat;
+}
+
+function prependCat(){
+    const prependCat = [cats.unshift(), "Arnold"];
+    return prependCat;
+}
+
+function removeLastCat(){
+    const removeLastCat = [...removeLastCat.slice(2)];
+    return removeLastCat
+}
